ae9ed68a8d48809e8dd9dfbaba894a8432c0ff76
Paul Stack <paulstack@hotmail.com>
2011-04-11 20:50:50 +0100

    refactoring of the file creation to a new method for cleanliness

--- SchemaCreation/Program.cs
+++ SchemaCreation/Program.cs
@@ -14,37 +14,45 @@ namespace SchemaCreation
             var source = string.Format(@"Data Source={0}", args[0].Replace("-source:", ""));
             var target = string.Format(@"Data Source={0}", args[1].Replace("-target:", ""));
             var outputPath = args[2].Replace("-outputPath:", "");
-            CreateSqlCeDifference(source, target, outputPath);
+
+            CreateSqlDiffScript(source, target, outputPath);
         }
 
-        private static void CreateSqlCeDifference(string source, string target, string outputPath)
+        private static void CreateSqlDiffScript(string source, string target, string outputPath)
         {
             using (IRepository sourceRepository = new DB4Repository(source))
             {
-                var generator = new Generator4(sourceRepository);
+                var diffGenerator = new Generator4(sourceRepository);
                 using (IRepository targetRepository = new DB4Repository(target))
                 {
-                    SqlCeDiff.CreateDiffScript(sourceRepository, targetRepository, generator);
-
-                    OutputDiffScript(generator.GeneratedScript, outputPath);
+                    SqlCeDiff.CreateDiffScript(sourceRepository, targetRepository, diffGenerator);
+                    OutputDiffScript(diffGenerator.GeneratedScript, outputPath);
                 }
             }
         }
 
         private static void OutputDiffScript(string diffScript, string outputPath)
         {
-            string explain = @"-- This database diff script contains the following objects:
--- - Tables:  Any that are not in the destination
--- -          (tables that are only in the destination are not dropped)
--- - Columns: Any added, deleted, changed columns for existing tables
--- - Indexes: Any added, deleted indexes for existing tables
--- - Foreign keys: Any added, deleted foreign keys for existing tables
--- ** Make sure to test against a production version of the destination database! ** " + Environment.NewLine + Environment.NewLine;
-            
-            var tw = new StreamWriter(outputPath);
-            tw.Write(explain + diffScript);
-            tw.Flush();
-            tw.Close();
+            string warning = @"-- This database diff script contains the following objects:
+                               -- - Tables:  Any that are not in the destination
+                               -- -          (tables that are only in the destination are not dropped)
+                               -- - Columns: Any added, deleted, changed columns for existing tables
+                               -- - Indexes: Any added, deleted indexes for existing tables
+                               -- - Foreign keys: Any added, deleted foreign keys for existing tables
+                               -- ** Make sure to test against a production version of the destination database! ** " + Environment.NewLine + Environment.NewLine;
+
+            if (File.Exists(outputPath))
+                throw new ApplicationException("The output path already exists - please change the filename or delete the previous version");
+
+            CreateDiffFile(outputPath, warning, diffScript);
+        }
+
+        private static void CreateDiffFile(string outputPath, string warning, string diffScript)
+        {
+            var outputWriter = new StreamWriter(outputPath);
+            outputWriter.Write(warning + diffScript);
+            outputWriter.Flush();
+            outputWriter.Close();
         }
     }
 }
